6914e4b4e28dd4c173f65759f1ccef8397b0895b
zhaojunhui <gamedirty@hotmail.com>
2016-09-19 18:03:35 +0800

    change

--- app/src/main/java/com/example/sovnem/stickysnot/TestActivity.java
+++ app/src/main/java/com/example/sovnem/stickysnot/TestActivity.java
@@ -2,6 +2,9 @@ package com.example.sovnem.stickysnot;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.view.Gravity;
+import android.view.View;
 
 import com.gamedirty.snotviewlib.SnotPanel;
 
@@ -15,5 +18,21 @@ public class TestActivity extends AppCompatActivity {
         SnotPanel root = SnotPanel.attachToWindow(this);
         root.makeViewSoft(this, R.id.snot);
         root.makeViewSoft(this,R.id.lalalal);
+        root.makeViewSoft(this,R.id.rect);
+
+
+    }
+
+    public void showPop(View v){
+        PopupMenu popupMenu = new PopupMenu(this,v, Gravity.START|Gravity.BOTTOM);
+        popupMenu.inflate(R.menu.lalala);
+        popupMenu.show();
     }
 }
+
+
+
+
+
+
+
